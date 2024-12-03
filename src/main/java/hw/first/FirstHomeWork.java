15bd69841a9e25ba52bf3725e219bb70a2166e91
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2024-12-02 22:30:23 -0300

    First HW

---
+++ src/main/java/hw/first/FirstHomeWork.java
@@ -0,0 +1,230 @@
+package hw.first;
+
+import java.util.*;
+
+public class FirstHomeWork {
+    /**
+     * Проверка делимости на 5 и 7 одновременно
+     *
+     * @param value - целое число
+     * @return true - если делится на 5 и 7 одновременно, иначе false
+     */
+    public boolean isMultiplicityMultipleOfFiveAndSeven(int value) {
+        return (value % 5 == 0) && (value % 7 == 0);
+    }
+
+    /**
+     * Определение принадлежности числа интервалу
+     *
+     * @param value - целое число
+     * @return true - если число принадлежит интервалу [10, 20], иначе false
+     */
+    public boolean isBetweenTenAndTwenty(int value) {
+        return (value >= 10) && (value <= 20);
+    }
+
+    /**
+     * Проверка на квадрат числа
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return true - если одно из чисел является квадратом другого, иначе false
+     */
+    public boolean isExponentiation(int a, int b) {
+        return Math.pow(a, 2) == b || Math.pow(b, 2) == a;
+    }
+
+    /**
+     * Проверка последней цифры
+     *
+     * @param value - целое число
+     * @return true - если последняя цифра числа 5, иначе false
+     */
+    public boolean isTheLastDigitalMultipleOfFive(int value) {
+        return (value % 10 != 0) && (value % 5 == 0);
+    }
+
+    /**
+     * Проверка, является ли сумма цифр четной
+     *
+     * @param value - целое число
+     * @return true - если сумма цифр числа четная, иначе false
+     */
+    public boolean isSumEven(int value) {
+        if (value < 0) {
+            value = Math.abs(value);
+        }
+
+        var alphabeticValue = "" + value;
+        List<String> stringList = Arrays.asList(alphabeticValue.split(""));
+
+        var sum = stringList.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        return sum % 2 == 0;
+    }
+
+    /**
+     * Сравнение двух чисел по модулю
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return true - если числа по модулю равны, иначе false
+     */
+    public boolean isAbsValueEquals(int a, int b) {
+        return Math.abs(a) == Math.abs(b);
+    }
+
+    /**
+     * Проверка знака числа
+     *
+     * @param value - целое число
+     * @return "Zero" - если число равно 0, "Negative" - если число отрицательное,
+     * "Positive" - если число положительное
+     */
+    public String getDefinitionOfNumber(int value) {
+        if (value == 0) {
+            return "Zero";
+        } else if (value < 0) {
+            return "Negative";
+        } else {
+            return "Positive";
+        }
+    }
+
+    /**
+     * Проверка делимости на 2 или 3
+     *
+     * @param value - целое число
+     * @return true - если число делится на 2 или 3, иначе false
+     */
+    public boolean isMultiplicityMultipleOfTwoOrTree(int value) {
+        return (value % 2 == 0) || (value % 3 == 0);
+    }
+
+    /**
+     * Четность суммы двух чисел
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return "Even" - если сумма чисел четная, "Odd" - если сумма чисел нечетная
+     */
+    public String getSumIsEvenOrOdd(int a, int b) {
+        return (a + b) % 2 == 0 ? "Even" : "Odd";
+    }
+
+    /**
+     * Определение числа, кратного и 4, и 6
+     *
+     * @param value - целое число
+     * @return true - если число кратно 4 и 6 одновременно, иначе false
+     */
+    public boolean isMultiplicityMultipleOfFourAndSix(int value) {
+        return (value % 4 == 0) && (value % 6 == 0);
+    }
+
+    /**
+     * Проверка суммы на четность и кратность 10
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return "Even and Divisible by 10" - если сумма четная и делится на 10,
+     * "Even but not Divisible by 10" - если сумма четная, но не делится на 10,
+     * "Odd" - если сумма нечетная
+     */
+    public String getDefinitionOfSum(int a, int b) {
+        var sum = a + b;
+        if (sum % 10 == 0) {
+            return "Even and Divisible by 10";
+        } else if (sum % 2 == 0) {
+            return "Even but not Divisible by 10";
+        } else {
+            return "Odd";
+        }
+    }
+
+    /**
+     * Максимум трех чисел
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @param c - целое число
+     * @return максимальное число
+     */
+    public int getMaxValue(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+
+    /**
+     * Проверка на палиндромность числа
+     *
+     * @param value - целое число
+     * @return true - если число палиндром, иначе false
+     */
+    public boolean isPalindrome(int value) {
+        if(value < 0) {
+            return false;
+        }
+        var alphabeticValue = "" + value;
+        List<String> stringList = Arrays.asList(alphabeticValue.split(""));
+        for (int i = 0; i < stringList.size() / 2; i++) {
+            if (!stringList.get(i).equals(stringList.get(stringList.size() - i - 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Кратность чисел и их сумма
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return сумма чисел, если оба числа кратны 3,
+     * если одно из них кратно 3, вернется удвоенное значение второго числа,
+     * иначе null
+     */
+    public Integer multiplicityOfNumbersAndTheirSum(int a, int b) {
+        if (a % 3 == 0 && b % 3 == 0) {
+            return a + b;
+        } else if (a % 3 == 0 && b % 3 != 0) {
+            return b * 2;
+        } else if (b % 3 == 0 && a % 3 != 0) {
+            return a * 2;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Проверка возраста на категорию
+     *
+     * @param value - целое число
+     * @return Child (до 12 лет), Teenager (от 13 до 17 лет), Adult (от 18 до 64 лет) или Senior (от 65 лет и старше)
+     */
+    public String getAgeCategory(int value) {
+        if (value < 0) {
+            return "Incorrect value";
+        } else if (value >= 0 && value <= 12) {
+            return "Child";
+        } else if (value >= 13 && value <= 17) {
+            return "Teenager";
+        } else if (value >= 18 && value <= 64) {
+            return "Adult";
+        } else {
+            return "Senior";
+        }
+    }
+
+    /**
+     * Проверка на делимость с остальным значением
+     *
+     * @param a - целое число
+     * @param b - целое число
+     * @return true - если число делится на остаток 2, иначе false
+     */
+    public boolean isDivisibleWithRemainderTwo(int a, int b) {
+        return a % b == 2;
+    }
+}
---
+++ src/test/java/FirstHomeWorkTest.java
@@ -0,0 +1,211 @@
+import hw.first.FirstHomeWork;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstHomeWorkTest {
+    private FirstHomeWork firstHomeWork;
+
+    @BeforeEach
+    void setUp() {
+        firstHomeWork = new FirstHomeWork();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "5, false",
+            "77, false",
+            "-35, true",
+            "0, true",
+            "175, true"
+    })
+    public void isMultiplicityMultipleOfFiveAndSevenTest(int value, boolean expected) {
+        var result = firstHomeWork.isMultiplicityMultipleOfFiveAndSeven(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-15, false",
+            "9, false",
+            "10, true",
+            "11, true",
+            "20, true",
+            "21, false"
+    })
+    public void isBetweenTenAndTwentyTest(int value, boolean expected) {
+        var result = firstHomeWork.isBetweenTenAndTwenty(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-2, 4, true",
+            "0, 0, true",
+            "4, 16, true",
+            "5, 50, false",
+            "-6, -36, false"
+    })
+    public void isExponentiationTest(int a, int b, boolean expected) {
+        var result = firstHomeWork.isExponentiation(a, b);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+                "-5, true",
+                "0, false",
+                "15, true",
+                "20, false"
+        })
+    public void isTheLastDigitalMultipleOfFiveTest(int value, boolean expected) {
+        var result = firstHomeWork.isTheLastDigitalMultipleOfFive(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-11, true",
+            "0, true",
+            "15, true",
+            "21, false"
+    })
+    public void isSumEvenTest(int value, boolean expected) {
+        var result = firstHomeWork.isSumEven(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, 5, true",
+            "0, 0, true",
+            "15, 17, false"
+    })
+    public void isAbsValueEqualsTest(int a, int b, boolean expected) {
+        var result = firstHomeWork.isAbsValueEquals(a, b);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, Negative",
+            "0, Zero",
+            "15, Positive"
+    })
+    public void getDefinitionOfNumberTest(int value, String expected) {
+        var result = firstHomeWork.getDefinitionOfNumber(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-33, true",
+            "0, true",
+            "6, true"
+    })
+    public void isMultiplicityMultipleOfTwoOrTreeTest(int value, boolean expected) {
+        var result = firstHomeWork.isMultiplicityMultipleOfTwoOrTree(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, 5, Even",
+            "0, 0, Even",
+            "1, 13, Even",
+            "2, 13, Odd"
+    })
+    public void getSumIsEvenOrOddTest(int a, int b, String expected) {
+        var result = firstHomeWork.getSumIsEvenOrOdd(a, b);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-24, true",
+            "0, true",
+            "6, false",
+            "96, true"
+    })
+    public void isMultiplicityMultipleOfFourAndSixTest(int value, boolean expected) {
+        var result = firstHomeWork.isMultiplicityMultipleOfFourAndSix(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, 5, Even and Divisible by 10",
+            "0, 0, Even and Divisible by 10",
+            "-1, 13, Even but not Divisible by 10",
+            "0, 13, Odd"
+    })
+    public void getDefinitionOfSumTest(int a, int b, String expected) {
+        var result = firstHomeWork.getDefinitionOfSum(a, b);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, 0, 5, 5",
+            "0, 0, 0, 0",
+            "-1, -1, -3, -1"
+    })
+    public void getMaxValue(int a, int b, int c, int expected) {
+        var result = firstHomeWork.getMaxValue(a, b, c);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-242, false",
+            "0, true",
+            "66, true",
+            "1457541, true",
+            "123421, false"
+    })
+    public void isPalindromeTest(int value, boolean expected) {
+        var result = firstHomeWork.isPalindrome(value);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-3, 33, 30",
+            "0, 2, 4",
+            "-1, 3, -2",
+            "7, 8, "
+    })
+    public void multiplicityOfNumbersAndTheirSumTest(int a, int b, Integer expected) {
+        var result = firstHomeWork.multiplicityOfNumbersAndTheirSum(a, b);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1, Incorrect value",
+            "0, Child",
+            "12, Child",
+            "13, Teenager",
+            "17, Teenager",
+            "18, Adult",
+            "64, Adult",
+            "65, Senior"
+    })
+    public void getAgeCategoryTest(int age, String expected) {
+        var result = firstHomeWork.getAgeCategory(age);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-5, 3, false",
+            "0, 2, false",
+            "11, 9, true"
+    })
+    public void isDivisibleWithRemainderTwoTest(int a, int b, boolean expected) {
+        var result = firstHomeWork.isDivisibleWithRemainderTwo(a, b);
+        assertEquals(expected, result);
+    }
+}
